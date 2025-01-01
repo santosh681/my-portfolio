ed345ba3f369edccaa71863274a6f948c7cc058a
santosh681 <rsantosh681@gmail.com>
2025-01-01 16:55:10 +0530

    Initial commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import About from './components/About';
+import About from './Components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
