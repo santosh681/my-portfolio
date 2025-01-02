5894082940054a2c2af8273b756962c4b9288647
santosh681 <rsantosh681@gmail.com>
2025-01-02 07:49:20 +0530

    final

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import Header from './components/Header';
-import About from './Components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <About />
-                <Projects />
-                <Contact />
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
