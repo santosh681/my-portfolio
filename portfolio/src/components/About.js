f9aaa1eb7136d1351bac971748880d8cf88a68e1
santosh681 <rsantosh681@gmail.com>
2025-01-06 12:55:58 +0530

    Version 4

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -3,9 +3,13 @@ import './About.css';
 
 function About() {
     return (
+        
         <section id="about" className="about">
             <h2>About Me</h2>
-            <p>Brief description about yourself and your skills.</p>
+            <img className="img-fluid" 
+     src={`${process.env.PUBLIC_URL}/assets/1686289711228.jpg`} 
+     alt="logo"/>
+            <p>Experienced IT Engineer with a demonstrated history of working in the information technology and services industry. SWIFT Certified Alliance Access Administrator and RMA operator.Skilled in Python, SFDC Development, SQL. Trained in Oracle Siebel CRM and Siebel Open UI. Strong information technology professional with a Bachelor of Engineering (B.E.) focused in Computer Science from sri lakshmi ammal engineering college.</p>
         </section>
     );
 }
